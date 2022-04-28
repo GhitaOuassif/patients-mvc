949c4c1bdec175ef711cef9dc9efdce27c649cb1
ghita <ouassif.ghita@gmail.com>
2022-04-28 12:05:02 +0100

    Partie 2 : Page template, Ajout des patients, validation des formulaires, édition et mise à jour des patients

--- src/main/java/ma/emsi/patientsmvc/entities/Patient.java
+++ src/main/java/ma/emsi/patientsmvc/entities/Patient.java
@@ -3,6 +3,7 @@ package ma.emsi.patientsmvc.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -12,9 +13,13 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min=4, max=40)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 }
--- src/main/java/ma/emsi/patientsmvc/repositories/PatientRepository.java
+++ src/main/java/ma/emsi/patientsmvc/repositories/PatientRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends JpaRepository
+        <Patient, Long> {
     Page<Patient> findByNomContains(String kw, Pageable pageable);
 }
